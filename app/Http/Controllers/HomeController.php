1d788cde01ef8acf7465dafad34d31aa8e787da4
Selim1710 <shamimaldeen@gmail.com>
2023-05-07 15:59:40 +0600

    form validation

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('home');
+    }
+    public function form()
+    {
+        return view('form');
+    }
+    public function store(Request $request)
+    {
+        // return $request->all();
+
+        $request->validate([
+            'name'=>'required|alpha',
+            'username'=>'required|regex:/^\S*$/u|regex:/^[\pL\s\-]+$/u',  // for white space + special character
+        ]);
+        return $request->all();
+
+    }
+}
